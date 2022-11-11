6920ad95fa84e9bc9dad940e618ffce59b6e4bf2
Sebastian Urdanegui Bisalaya <sebasurdanegui@gmail.com>
2022-11-11 14:14:59 -0500

    Solution a problem upload.

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import re
 
 from setuptools import setup
 from setuptools import find_packages
+from setuptools import Extension
 
 def read(filename):
     filename = os.path.join(os.path.dirname(__file__), filename)
